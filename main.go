fd7b3a5d8214b7d245eaf4d2b6203ed044a0d85d
@skylerbair <skylertimothybair@hotmail.com>
2021-12-23 16:25:32 -0700

    added func for Kelvin

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
 	}
+
 	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
@@ -30,10 +31,29 @@ func main() {
 			printError(errReadingInput)
 		}
 		if originUnit == "C" {
-			convertToFahrenheit(originValue)
-		} else {
-			convertToCelsius(originValue)
+			convertCToFahrenheit(originValue)
+			//adding convert to K to this if
+			convertCToKelvin(originValue)
+
 		}
+		//added a if for F
+		if originUnit == "F" {
+			convertFToCelsius(originValue)
+			convertFToKelvin(originValue)
+		}
+		//added a if for K
+		if originUnit == "K" {
+			convertKToCelsius(originValue)
+			convertKTofahrenheit(originValue)
+		}
+		/* Dont think i need a else im just going to have more if's
+		 else {
+			convertFToCelsius(originValue)
+			convertCToKelvin(originValue)
+		 }
+		*/
+		//^	added convertToKelvin(originValue) ^
+
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 		_, err = fmt.Scanln(&shouldConvertAgain)
 		if err != nil {
@@ -52,12 +72,38 @@ func printError(err error) {
 	os.Exit(1)
 }
 
-func convertToCelsius(value float64) {
+//changed to convertFToCelsius from convertToCelsius
+func convertFToCelsius(value float64) {
 	convertedValue := (value - 32) * 5 / 9
+	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
+}
+
+//Addded F-K converter
+func convertFToKelvin(value float64) {
+	convertedValue := (value-32)*5/9 + 273.15
+	fmt.Printf("%v K = %.0f F\n", value, convertedValue)
+}
+
+//changed to convertCToFahrenheit from convertTOFahrenheit
+func convertCToFahrenheit(value float64) {
+	convertedValue := (value * 9 / 5) + 32
 	fmt.Printf("%v F = %.0f C\n", value, convertedValue)
 }
 
-func convertToFahrenheit(value float64) {
-	convertedValue := (value * 9 / 5) + 32
-	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
+//  add c-k func
+func convertCToKelvin(value float64) {
+	convertedValue := (value + 273.15)
+	fmt.Printf("%v K = %.0f C\n", value, convertedValue)
+}
+
+// added k-f converter
+func convertKTofahrenheit(value float64) {
+	convertedValue := (value-273.15)*9/5 + 32
+	fmt.Printf("%v F = %.0f K\n", value, convertedValue)
+}
+
+// added K-C converter
+func convertKToCelsius(value float64) {
+	convertedValue := (value - 273.15)
+	fmt.Printf("%v C = %.0f K\n ", value, convertedValue)
 }
